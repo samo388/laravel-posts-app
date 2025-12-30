74c20004c4cc034774ca6cf14bc2a353c52705c9
samo388 <mohamedbahr3836@gmail.com>
2025-12-30 12:38:32 +0200

    Add API auth, posts relations, and CORS config

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,31 +5,32 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use App\Models\Post;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
+   
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
 
+    
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
+  
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
 
+    
     public function posts()
     {
         return $this->hasMany(Post::class);
